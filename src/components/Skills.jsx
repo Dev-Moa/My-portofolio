a7126f980ee4f4d22271d3564cb2ce17b28d95be
Dev Moha <113305590+Moha-Shrf@users.noreply.github.com>
2022-12-04 11:43:50 +0300

    Fa changed fa

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiFillHtml5 } from "react-icons/ai";
 import { DiCss3 } from "react-icons/di";
 import { SiJavascript } from "react-icons/si";
-import { FaReact } from "react-icons/Fa";
+import { FaReact } from "react-icons/fa";
 import { SiFirebase } from "react-icons/si";
 import { IoLogoPython } from "react-icons/io5";
 import { SiTypescript, SiRedux, SiDjango } from "react-icons/si";
