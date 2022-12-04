b2f5a0a4b708ab5e94cfc6a46590302d9d7dfe26
Dev Moha <113305590+Moha-Shrf@users.noreply.github.com>
2022-12-04 12:56:21 +0300

    socialmedia

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { AiFillFacebook, AiFillGithub } from "react-icons/ai";
+import { BsYoutube } from "react-icons/bs";
 function Navbar() {
   const style = {
     container: `flex mb-12 justify-between items-center text-white  `,
@@ -11,6 +13,7 @@ function Navbar() {
     menu: `text-3xl  md:hidden`,
     grdtxt: `font-semibold hero text-transparent text-2xl md:text-4xl bg-clip-text bg-gradient-to-r from-[#F80505] to-[#FF7B36]`,
     li: `hover:bg-gray-400 hover:p-1 rounded`,
+    tech: `text-3xl text-white flex flex-col md:flex-row fixed top-10 right-5 bg-gray-800 p-2 gap-4 rounded`,
   };
   const [nav, setNav] = useState(true);
   const handleClick = () => {
@@ -20,33 +23,26 @@ function Navbar() {
   return (
     <div className={style.container}>
       <h1 className={style.grdtxt}>Dev Moha</h1>
-
-      <ul className={nav ? style.ul : style.ul_mobile}>
-        <Link to="about">
-          <li className={style.li}>About</li>
-        </Link>
-        <Link to="skills">
-          <li className={style.li}>Skills</li>
-        </Link>
-
-        <Link to="education">
-          <li className={style.li}>Education</li>
-        </Link>
-        <Link to="projects">
-          <li className={style.li}>Projects</li>
-        </Link>
-        <Link to="contact">
-          <li className={style.li}>Contact</li>
-        </Link>
-      </ul>
-
-      <button onClick={handleClick} className={style.btn}>
-        {nav ? (
-          <IoMenu className={style.menu} />
-        ) : (
-          <IoMdClose className={style.menu} />
-        )}
-      </button>
+      <div className={style.tech}>
+        {/* facebook */}
+        <a
+          target="_blank"
+          href="https://www.facebook.com/profile.php?id=100085252966518"
+        >
+          <AiFillFacebook />
+        </a>
+        {/* youtube */}
+        <a
+          target="_blank"
+          href="https://www.youtube.com/channel/UCp0Mg7lHe2h_cP8Dl9leBqg"
+        >
+          <BsYoutube />
+        </a>
+        {/* github */}
+        <a target="_blank" href="https://github.com/Dev-Moa">
+          <AiFillGithub />
+        </a>
+      </div>
     </div>
   );
 }
